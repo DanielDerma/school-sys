087bd996069e300424bdb09cc5dc4088ed1d6e00
DanielDerma <danielderma94@gmail.com>
2022-09-26 20:01:12 -0600

    deploy5

--- src/services/admin.js
+++ src/services/admin.js
@@ -2,10 +2,10 @@ let admin = require("firebase-admin");
 
 let serviceAccount = {
   type: process.env.ADMIN_TYPE || "",
-  project_id: process.env.ADMIN_PROJECT_ID || "",
+  project_id: process.env.ADMIN_PROJECT_ID || "test-project",
   private_key_id: process.env.ADMIN_PRIVATE_KEY_ID || "",
-  private_key: process.env.ADMIN_PRIVATE_KEY || "",
-  client_email: process.env.ADMIN_CLIENT_EMAIL || "",
+  private_key: process.env.ADMIN_PRIVATE_KEY || "REDACTED",
+  client_email: process.env.ADMIN_CLIENT_EMAIL || "REDACTED",
   client_id: process.env.ADMIN_CLIENT_ID || "",
   auth_uri: process.env.ADMIN_AUTH_URI || "",
   token_uri: process.env.ADMIN_TOKEN_URI || "",
