f853f9f58a874f0635ff31aba6f3b8bc3699545d
DanielDerma <danielderma94@gmail.com>
2022-09-26 19:57:10 -0600

    deploy4

--- src/services/admin.js
+++ src/services/admin.js
@@ -1,17 +1,17 @@
 let admin = require("firebase-admin");
 
 let serviceAccount = {
-  type: process.env.ADMIN_TYPE,
-  // project_id: process.env.ADMIN_PROJECT_ID,
-  project_id: "prueba1-4ba9d",
-  private_key_id: process.env.ADMIN_PRIVATE_KEY_ID,
-  private_key: process.env.ADMIN_PRIVATE_KEY,
-  client_email: process.env.ADMIN_CLIENT_EMAIL,
-  client_id: process.env.ADMIN_CLIENT_ID,
-  auth_uri: process.env.ADMIN_AUTH_URI,
-  token_uri: process.env.ADMIN_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.ADMIN_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.ADMIN_CLIENT_X509_CERT_URL,
+  type: process.env.ADMIN_TYPE || "",
+  project_id: process.env.ADMIN_PROJECT_ID || "",
+  private_key_id: process.env.ADMIN_PRIVATE_KEY_ID || "",
+  private_key: process.env.ADMIN_PRIVATE_KEY || "",
+  client_email: process.env.ADMIN_CLIENT_EMAIL || "",
+  client_id: process.env.ADMIN_CLIENT_ID || "",
+  auth_uri: process.env.ADMIN_AUTH_URI || "",
+  token_uri: process.env.ADMIN_TOKEN_URI || "",
+  auth_provider_x509_cert_url:
+    process.env.ADMIN_AUTH_PROVIDER_X509_CERT_URL || "",
+  client_x509_cert_url: process.env.ADMIN_CLIENT_X509_CERT_URL || "",
 };
 
 if (!admin.apps.length) {
